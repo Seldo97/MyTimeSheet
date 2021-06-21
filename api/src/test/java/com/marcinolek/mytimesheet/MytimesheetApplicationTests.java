74f16370b2cfdbb0f3bdfb8d78653a1eb327bccb
Seldo97 <seldo97@gmail.com>
2021-06-21 23:13:05 +0200

    init repo

---
+++ api/src/test/java/com/marcinolek/mytimesheet/MytimesheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcinolek.mytimesheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytimesheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
