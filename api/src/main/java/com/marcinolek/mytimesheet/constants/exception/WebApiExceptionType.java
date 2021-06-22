f2744d2f96e035449238f12e63339f6021e60838
Seldo97 <seldo97@gmail.com>
2021-06-22 23:14:53 +0200

    Added user_group table and relation with users table

---
+++ api/src/main/java/com/marcinolek/mytimesheet/constants/exception/WebApiExceptionType.java
@@ -0,0 +1,7 @@
+package com.marcinolek.mytimesheet.constants.exception;
+
+public enum WebApiExceptionType {
+    USER_UNAUTHORIZED,
+    BAD_CREDENTIALS,
+    NO_PERMISSION_TO_RESOURCE
+}
---
+++ api/src/main/java/com/marcinolek/mytimesheet/constants/permission/PermissionGroup.java
@@ -0,0 +1,12 @@
+package com.marcinolek.mytimesheet.constants.permission;
+
+public enum PermissionGroup {
+    ADMIN,
+    USER,
+    USER_GROUP,
+    PERMISSION,
+    ORG_UNIT,
+    PROJECT,
+    DICTIONARY,
+    TASK
+}
